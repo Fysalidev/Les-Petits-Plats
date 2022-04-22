db1cd8486850f00a327209ce539e44a6dd7203de
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-04-22 17:50:24 +0200

    Vendredi soir test sur class BtnFilter...

---
+++ js/utils/FilterTagsBtnFactory.js
@@ -0,0 +1,29 @@
+class FilterTagsBtnFactory {
+
+    constructor(catalogue, filterTagsBtnType) {
+        this.catalogue = catalogue;
+        this.filterTagsBtnType = filterTagsBtnType;
+    }
+
+    createBtn()  {
+        switch (this.filterTagsBtnType) {
+            case "ingredient":
+                return new FilterBtn(this.catalogue.ingredients, "ingredient");
+                break;
+
+            case "appliance":
+                return new FilterBtn(this.catalogue.appliances, "appliance");
+                break;
+
+            case "ustensil":
+                return new FilterBtn(this.catalogue.ustensils, "ustensil");
+                break;
+
+            default:
+                console.log("error : type not found");
+                break;
+        }
+    }
+
+    }
+}
